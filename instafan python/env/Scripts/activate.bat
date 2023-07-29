b57396efd5126d51671e80800ee3a25751707514
Adrian Turski <adrian.turski91@onet.pl>
2023-07-29 16:47:28 +0200

    instafan-python164629072023package

--- instafan python/env/Scripts/activate.bat
+++ instafan python/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\adria\Desktop\projects\instafan python\env
+set VIRTUAL_ENV=C:\Users\adria\Desktop\projects\instafan\instafan python\env
 
 if not defined PROMPT set PROMPT=$P$G
 
